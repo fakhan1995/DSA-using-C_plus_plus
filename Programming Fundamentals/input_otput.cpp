eff8ff4205c1da8e89c148ec82b30ff31469d404
Faizan Ahmad Khan <59918587+faizanahmadkhan1@users.noreply.github.com>
2024-03-24 04:54:34 +0530

    Programming fundamentals

---
+++ Programming Fundamentals/input_otput.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int amt1, amt2;
+    cin >> amt1, amt2;
+
+    int sum;
+    sum = amt1 + amt2;
+    cout << "sum\n";
+    cout << sum;
+
+    return 0;
+}
